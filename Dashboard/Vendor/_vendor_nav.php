f8172c0565569cae233df4322580b0636f5948ba
Bhuvnesh singh <cebhuvneshsingh@gmail.com>
2025-04-27 12:20:21 +0530

    minor change

--- Dashboard/Vendor/_vendor_nav.php
+++ Dashboard/Vendor/_vendor_nav.php
@@ -9,7 +9,6 @@
         crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    
 </head>
 
 <body>
